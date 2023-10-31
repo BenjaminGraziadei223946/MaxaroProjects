943347c1fad6e52f6c1f03ef7b95188999c75d67
BenjaminGraziadei223946 <223946@buas.nl>
2023-10-31 10:08:38 +0100

    a

---
+++ function_app.py
@@ -0,0 +1,19 @@
+import logging
+import azure.functions as func
+import json
+
+
+app = func.FunctionApp()
+
+@app.event_grid_trigger(arg_name="azeventgrid")
+def AcceptCall(event: func.EventGridEvent):
+
+    result = json.dumps({
+        'id': event.id,
+        'data': event.get_json(),
+        'topic': event.topic,
+        'subject': event.subject,
+        'event_type': event.event_type,
+    })
+
+    logging.info('Python EventGrid trigger processed an event %s', result)
