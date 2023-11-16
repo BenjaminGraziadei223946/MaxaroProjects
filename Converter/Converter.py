7b7d28be7a06e03a41251c81fb39ec6e5ea1e670
BenjaminGraziadei223946 <223946@buas.nl>
2023-11-16 11:01:17 +0100

    Update Converter.py

--- Converter/Converter.py
+++ Converter/Converter.py
@@ -24,7 +24,7 @@ def replace_words_v2(description, row):
         return description
     
     for header in df.columns:
-        st.write(f'[{header}] = {value}')
+        st.write(f'[{header}]')
         if header in description:
             value = row[header]
             
