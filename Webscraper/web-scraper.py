4d5171f656b30e3d51b0b5d4998678f41fbd4a65
BenjaminGraziadei223946 <223946@buas.nl>
2024-01-12 13:06:18 +0100

    Update web-scraper.py

--- Webscraper/web-scraper.py
+++ Webscraper/web-scraper.py
@@ -44,7 +44,8 @@ def generate_description(url, soup):
 
             response = openai.Completion.create(
                 model="gpt-35-turbo",
-                prompt = f'Maak een overtuigende en positieve productbeschrijving voor het volgende artikel. Benadruk de belangrijkste kenmerken, voordelen en onderscheidende kenmerken. Gebruik duidelijke en begrijpelijke taal om de lezer te boeien. Stel je voor dat je tegen een potentiële klant spreekt die op zoek is naar de beste kwaliteiten van het product. Maak de beschrijving ongeveer 150-200 woorden. {text}',
+                prompt = f"Maak een overtuigende en positieve productbeschrijving voor het volgende artikel. Benadruk de belangrijkste kenmerken, voordelen en onderscheidende kenmerken. Gebruik duidelijke en begrijpelijke taal om de lezer te boeien. Stel je voor dat je tegen een potentiële klant spreekt die op zoek is naar de beste kwaliteiten van het product. Maak de beschrijving ongeveer 150-200 woorden. {text}",
+                max_tokens=150
             )
             new_row = {'Product': product_title, 'Description': response.choices[0].text, 'URL': url}
             df_prodDes.loc[len(df_prodDes)] = new_row
